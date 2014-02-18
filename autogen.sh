e498b9f33690ad4e7925f3ecf64e4bd98a58bb9c
Nathan Geffen <nathangeffen@gmail.com>
2014-02-18 23:22:19 +0200

    Create m4 if does not exit

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -e
 test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
+test -e m4 || mkdir m4
 
 autoreconf --force --install --verbose "$srcdir"
 test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
